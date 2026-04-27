202f1bb9fb4bf5979088b1bdb0bd14c8febe9627
namfah22list <nampha2551@gmail.com>
2026-04-27 14:48:31 +0700

    หห

--- jamgame/Assets/code2/MainMenuUI.cs
+++ jamgame/Assets/code2/MainMenuUI.cs
@@ -1,9 +1,21 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class MainMenuUI : MonoBehaviour
 {
+    public GameObject videoScreen;
+    public VideoPlayer videoPlayer;
+
     public void StartGame()
+    {
+        videoScreen.SetActive(true);
+        videoPlayer.Play();
+
+        videoPlayer.loopPointReached += OnVideoEnd;
+    }
+
+    void OnVideoEnd(VideoPlayer vp)
     {
         SceneManager.LoadScene("jingjoak");
     }
